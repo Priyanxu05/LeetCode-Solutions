7dfa910bf97bb691493c6747c9e69ff4172f525f
Priyanxu <priyanshuexe044@gmail.com>
2026-09-01 20:51:58 +0530

    Time: 0 ms (100%), Space: 45.4 MB (77.45%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -20,8 +20,9 @@ class Solution {
     public int checkthatshii(TreeNode root){
         if(root == null)return 0;
         int lh=checkthatshii(root.left);
+        if(lh ==-1)return -1;
         int rh=checkthatshii(root.right);
-        if(lh ==-1 || rh==-1)return -1;
+        if(rh==-1)return -1;
         if(Math.abs(lh - rh ) > 1)return -1;
 
         return Math.max(lh,rh)+1;
