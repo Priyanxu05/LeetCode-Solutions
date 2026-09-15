5fb60adfdcf04839514c6c5f17dbdd1819da07bc
Priyanxu <priyanshuexe044@gmail.com>
2026-09-15 09:02:18 +0530

    Time: 10 ms (44.07%), Space: 46.8 MB (89.94%) - LeetHub

---
+++ 0707-design-linked-list/0707-design-linked-list.java
@@ -0,0 +1,94 @@
+class MyLinkedList {
+        class Node {
+        int data;
+        Node next;
+
+        Node(int val) {
+            this.data=val;
+            this.next=null;
+        }
+    }
+
+    int size;
+    Node head;
+
+    public MyLinkedList() {
+        head=null;
+        size=0;
+    }
+
+    public int get(int index) {
+        if(index<0||index>=size)
+                return -1;
+
+        Node temp=head;
+        for(int i=0;i<index;i++)
+        temp=temp.next;
+
+        return temp.data;
+
+    }
+    
+    public void addAtHead(int val) {
+        Node newnode=new Node(val);
+        newnode.next=head;
+        head=newnode;
+        size++;
+    }
+    
+    public void addAtTail(int val) {
+        Node newnode=new Node(val);
+        if(head == null){
+            head=newnode;
+            size++;
+            return;
+        }
+        Node temp=head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next=newnode;
+        size++;
+
+    }
+    
+    public void addAtIndex(int index, int val) {
+        if(index<0||index>size)return;
+
+        if(index==0){
+            addAtHead(val);
+            return;
+        }
+        Node newnode=new Node(val);
+        Node temp=head;
+        for(int i=0;i<index-1;i++)temp=temp.next;
+
+        newnode.next=temp.next;
+        temp.next=newnode;
+        size++;
+    }
+    
+    public void deleteAtIndex(int index) {
+        if(index<0||index>=size)return;
+        if(index==0){
+            head = head.next;
+            size--;
+            return;
+        }
+
+        Node temp=head;
+        for(int i=0;i<index-1;i++)temp=temp.next;
+        temp.next=temp.next.next;
+        size--;
+    }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
